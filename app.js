9c1fa7d874df94d64f5f5bc0ba827e05dda98442
Tyler C Smith <tylercsmith94@gmail.com>
2021-03-16 22:43:17 +0700

    Adds SVG imgs, basic layout

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// MODULES
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -5,10 +6,16 @@ const connectFlash = require('connect-flash');
 require('dotenv').config();
 const PORT = process.env.PORT;
 
+// VIEW ENGINE
+app.set('view engine', 'ejs');
+
+
+/// ROUTES
+
 app.get('/', (req, res) => {
-  res.send('sanity check');
+  res.render('index');
 })
 
 
-
+//LISTENER
 app.listen(PORT, () => console.log(`I'm listening on port: ${PORT}`));
