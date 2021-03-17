9c79ce810af349dedea7392daf9165fb6fa376c2
Tyler C Smith <tylercsmith94@gmail.com>
2021-03-18 01:12:32 +0700

    Revamp ui for landing

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const connectFlash = require('connect-flash');
+const methodOverride = require('method-override')
 require('dotenv').config();
 const PORT = process.env.PORT;
 
 // VIEW ENGINE
 app.set('view engine', 'ejs');
 
+//MIDDLEWARE
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+  //MORGAN replacement
+app.use((req, res, next) => {
+  const method = req.method;
+  const path = req.url;
+  const timestamp = new Date().toLocaleTimeString();
+  console.log(`${method} ${path} ${timestamp}`);
+  next(); 
+});
 
 /// ROUTES
 
