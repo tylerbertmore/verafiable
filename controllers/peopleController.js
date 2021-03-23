7f038ea3d54638b43dce9a9938c9a7b0500318c5
tylerbertmore <tylercsmith94@gmail.com>
2021-03-24 02:51:37 +0700

    Show/ Edit Sanity Checks

--- controllers/peopleController.js
+++ controllers/peopleController.js
@@ -21,6 +21,19 @@ router.get('/', isLoggedIn, (req, res) => {
   })
 })
 
+router.get('/:userId', isLoggedIn, (req, res) => {
+  db.Person.findById(req.params.userId, (err, foundUser) => {
+    if(err) return console.log(err)
+    res.render('people/show', {person: foundUser})
+  })
+})
+router.get('/:userId/edit', isLoggedIn, (req, res) => {
+  db.Person.findById(req.params.userId, (err, foundUser) => {
+    if(err) return console.log(err)
+    res.render('people/edit', {person: foundUser})
+  })
+})
+
 
 
 module.exports = router
